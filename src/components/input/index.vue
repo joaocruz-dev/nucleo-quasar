47d02ee06dff5ce23010a7e2e2e6eb95fd2581eb
João Cruz <jcruz@soluts.co>
2025-03-14 01:13:06 -0300

    upgraded phone mask, new version

--- src/components/input/index.vue
+++ src/components/input/index.vue
@@ -74,8 +74,10 @@ export default {
       if (this.type === 'date') return '##/##/####'
       if (this.type === 'cpf') return '###.###.###-##'
       if (this.type === 'phone') {
-        if (this.model.length < 15) return '(##) ####-####'
-        else return  '(##) #####-####'
+        if (!this.model) return '(##) #####-####'
+        if (this.model.length < 9) return '(##) #####-####'
+        if (this.model.length < 15) return '(##) ####-#####'
+        return '(##) #####-####'
       }
       if (this.type === 'cnpj') return '##.###.###/####-##'
       if (this.type === 'color') return '\\#XXXXXX'
